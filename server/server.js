b643a7bdeb8bb232a0ef5c5265c1c1e97ee4c01d
Corentin Grall <corentin.grall@gmail.com>
2018-07-23 12:44:34 +0200

    Fixed port for deploying

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const publicPath = path.join(__dirname, '../public');
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
-});
+});dc
