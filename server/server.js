eaec1dc2be373a22a2a6e0481d18740297206e85
Corentin Grall <corentin.grall@gmail.com>
2018-07-31 11:41:32 +0200

    Created generateMessage utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -13,19 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'corentin.grall@gmail.com',
-        text: 'Hey, how are you ?',
-        createdAt: 123
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+    
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+         io.emit('newMessage', {})
     });
 
     socket.on('disconnect', () => {
