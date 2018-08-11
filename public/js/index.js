57b52a53a9f00f059374863bc5348bd705a2a434
Corentin Grall <corentin.grall@gmail.com>
2018-08-11 15:40:52 +0200

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+    if(clientHeight + scrollTop  + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
 socket.on('connect', function (){
     console.log('Connected to Server');
 });
@@ -19,17 +37,10 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
-
-    // console.log('Received new Message', message);
-    // var formattedTime = moment(message.createdAt).format('h:mm a');
-    // var li = jQuery('<li></li>');
-    // li.text(`${message.from} - ${formattedTime} : ${message.text}`);
-
-    // jQuery('#messages').append(li);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
-    // var li = jQuery('<li></li>');
     var template = jQuery('#location-message-template').html();
 
     var formattedTime = moment(message.createdAt).format('h:mm a');
@@ -39,13 +50,7 @@ socket.on('newLocationMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
-
-    // var a = jQuery('<a target="_blank">My current location</a>');
-
-    // li.text(`${message.from} - ${formattedTime}: `);
-    // a.attr('href', message.url);
-    // li.append(a);
-    // jQuery('#messages').append(li);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
